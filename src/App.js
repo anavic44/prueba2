4b548d4a9c7adb0c7f7b7783f05fafbec5d861b4
anavic44 <a01567111@tec.mx>
2024-05-06 11:24:47 -0600

    sig commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
+
+import ARComponents from "./components/vanilla/ARComponents";
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { RegistrarProyecto } from "./RegisterScene";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path = '/' element = {<Login/>}></Route> */}
-        <Route path='/' element={<Login onLoginSuccess={handleLoginSuccess} />} />
-        <Route path = '/register' element = {<Register/>}></Route>
-        <Route path = '/navbar' element = {<NavBar/>}></Route>
-        {/* <Route path = '/projects' element = {<Projects2/>}></Route> */}
-        <Route path="/ar" element={<ARComponents />}></Route>
-        <Route path="/ar/:id_objeto" element={<ARComponents />}></Route>
-        <Route path='/projects/:userId' element={<Projects2 />} />
-
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <ARComponents />
+    </div>
   );
 }
 export default App;
-
-//      <ARComponents />
 
 
 /*
